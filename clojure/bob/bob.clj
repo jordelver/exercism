82557c0bbd70c6183cdfe216cf54a8fcb0fa2c3d
Jordan Elver <jordan.elver@gmail.com>
2013-08-03 16:22:59 +0100

    Refactor using methods and everything

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,18 +1,20 @@
 (ns bob)
 
+(defn shouting? [word]
+  (= word
+    (clojure.string/upper-case word)))
+
+(defn question? [word]
+  (string?
+    (re-find #"\?\z" word)))
+
+(defn silence? [word]
+  (= word ""))
+
 (defn response-for [speech]
   (cond
-    (= speech "") "Fine, be that way."
-    (= speech "Tom-ay-to, tom-aaaah-to.") "Whatever."
-    (= speech "Let's go make out behind the gym!") "Whatever."
-    (= speech "It's OK if you don't want to go to the DMV.") "Whatever."
-    (= speech "Ending with ? means a question.") "Whatever."
-    (= speech "1, 2, 3 GO!") "Woah, chill out!"
-    (= speech "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!") "Woah, chill out!"
-    (= speech "WATCH OUT!") "Woah, chill out!"
-    (= speech "I HATE YOU") "Woah, chill out!"
-    (= speech "Does this cryogenic chamber make me look fat?") "Sure."
-    (= speech "WHAT THE HELL WERE YOU THINKING?") "Woah, chill out!"
-  )
-)
+    (silence? speech) "Fine, be that way."
+    (shouting? speech) "Woah, chill out!"
+    (question? speech) "Sure."
+    :else "Whatever."))
 
